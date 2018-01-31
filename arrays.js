717b9f8a21fbfffab079ee095633c8bd8f406ed1
arye dov eidelman <aryedov.e@gmail.com>
2018-01-31 23:42:30 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+//add
+function addElementToBeginningOfArray(array, element){
+  array = [element, ...array];
+}
+function destructivelyAddElementToBeginningOfArray(array, element)){
+
+}
+function addElementToEndOfArray(array, element){
+
+}
+function destructivelyAddElementToEndOfArray(array, element){
+
+}
+
+//access
+function accessElementInArray(array, index){
+
+}
+
+//remove
+function destructivelyRemoveElementFromBeginningOfArray(array){
+
+}
+function removeElementFromBeginningOfArray(array){
+
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+
+}
+function removeElementFromEndOfArray(array){
+
+}
